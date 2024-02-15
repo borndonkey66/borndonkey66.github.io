d1efc0de086a6b3f955edc7ce9548e8579e2dde1
Req <shoumyareeq@gmail.com>
2024-02-15 11:35:10 +0600

    Learning 0
    Added buttons for adding, subtracting and resetting in Count.razor.
    Added Count.razor to Home.razor.

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaab39aee31a12ff6d503ec931020f66929615df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+256ae08405438753edde13810ea322b19449c5b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BlazorApp/obj/Release/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Release/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+973ec73df85c68613dd6d506b5e0f9318c237823")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+256ae08405438753edde13810ea322b19449c5b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
