ac71aeae010a911351c5a14874fd484eb95524b0
Req <shoumyareeq@gmail.com>
2024-02-16 13:25:10 +0600

    added a Todo.razor

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1efc0de086a6b3f955edc7ce9548e8579e2dde1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71ea51ee831758d602c4acd30faa06f7bcd5dd0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
