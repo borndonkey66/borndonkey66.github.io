3268c454c1a73fb9553f0d036504c6c8c0ac2bf1
Req <shoumyareeq@gmail.com>
2024-02-21 00:16:25 +0600

    changed website colors and navbar design

--- BlazorApp/obj/Release/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Release/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+453d2f186365a63c99cc6937e624c95594964647")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c947fc97ad220c4790d7b6b9592aa696e4dd6926")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
